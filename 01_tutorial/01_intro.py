9ce220df55f3131cf0ea04b984db224f4d97dd73
nabya18 <maulida.nabya@gmail.com>
2024-02-20 14:29:28 +0700

    rename file

---
+++ 01_tutorial/01_intro.py
@@ -0,0 +1,19 @@
+'''
+Download Python from the official Python web site: https://python.org
+
+Apa itu python? bahasa pemrograman yang dibuat oleh Guido van Rossum tahun 1991
+1. Digunakan untuk:
+    - web development
+    - software development
+    - matematika
+    - sistem script
+2. Apa yang bisa dilakukan python:
+    - dapat digunakan di server untuk membuat aplikasi web
+    - dapat digunakan bersama perangkat lunak untuk membuat alur kerja
+    - dapat digunakan ke sistem database. (dapat membaca dan memodifikasi file.)
+    - dapat digunakan untuk menangani data besar dan melakukan matematika yang kompleks.
+    - dapat digunakan untuk pembuatan prototipe cepat, atau untuk pengembangan perangkat lunak siap produksi.
+3. Mengapa python?
+    - berjalan pada sistem interpreter, artinya kode dapat dieksekusi segera setelah ditulis.
+    - dapat ditangani dengan cara prosedural, cara berorientasi objek, atau cara fungsional.
+'''
