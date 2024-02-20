751215057d36f9a4f43a546b50d8f2a3c45ca8e1
nabya18 <maulida.nabya@gmail.com>
2024-02-20 15:09:41 +0700

    menambahkan komentar

---
+++ 01_tutorial/03_comment.py
@@ -0,0 +1,14 @@
+'''
+1. Single comment => diawali #
+2. Multiple comments =>
+    """
+    This is a comment
+    written in
+    more than just one line
+    """
+'''
+
+#This is a comment
+print("comment 1")
+
+print("comment 2") #This is a comment
