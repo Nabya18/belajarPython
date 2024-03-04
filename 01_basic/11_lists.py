9cf98f18bbd6d5a22b1f72f3b0a71e1082a079a7
nabya18 <maulida.nabya@gmail.com>
2024-03-04 14:35:59 +0700

    update list

--- 01_basic/11_lists.py
+++ 01_basic/11_lists.py
@@ -15,12 +15,40 @@ List => digunakan untuk menyimpan banyak item dalam satu variabel
           - pop() => menghapus indeks yang ditentukan, Jika tidak menentukan indeks, pop()metode ini akan menghapus item terakhir.
           - del => menghapus indeks yang ditentukan dan dapat menghapus list sepenuhnya
         - Clear the List => clear(), listnya masih ada, tetapi tidak ada isinya
-    e. Loop Lists
-    f. List Comprehension
-    g. Sort Lists
+    e. Loop Lists => pengulangan dalam list
+        - for loop
+        - While Loop
+    f. List Comprehension => menawarkan sintaks yang lebih pendek
+        - newlist = [expression for item in iterable]
+        - Memanipulasi hasil dengan expression
+    g. Sort Lists => default ascending
+        - reverse = True => Untuk mengurutkan secara descending
+        - semua huruf kapital diurutkan sebelum huruf kecil
+        - key = str.lower => agar tidak case sensitive
+        - reverse() => membalik urutan pengurutan elemen saat ini.
     h. Copy Lists
-    i. Join Lists
+        tidak dapat copy list hanya dengan mengetik list2 = list1
+        karena list2hanya akan menjadi referensi ke list1
+        perubahan yang dilakukan list1secara otomatis juga akan dilakukan di list2
+        - copy() => untuk membuat salinan
+        - list()
+    i. Join Lists => menggabungkan list
+        - by using the + operator
+        - by appending all the items from list2 into list1
+        - extend() => menambahkan elemen dari satu daftar ke daftar lainnya
     j. List Methods
+        Method	            Description
+        append()	        Menambahkan elemen di akhir list
+        clear()	            Menghapus semua elemen dari list
+        copy()	            Mengembalikan salinan list
+        count()	            Mengembalikan jumlah elemen dengan nilai yang ditentukan
+        extend()	        Menambahkan elemen list (atau apa pun yang dapat diubah), ke akhir list saat ini
+        index()	            Mengembalikan indeks elemen pertama dengan nilai yang ditentukan
+        insert()	        Menambahkan elemen pada posisi yang ditentukan
+        pop()	            Menghapus elemen pada posisi yang ditentukan
+        remove()	        Menghapus item dengan nilai yang ditentukan
+        reverse()	        Membalikkan urutan list
+        sort()	            Mengurutkan list
 
 Karakteristik List:
     1. diurutkan => memiliki urutan yang ditentukan, dan urutan itu tidak akan berubah.
@@ -122,3 +150,93 @@ del thislist3 #menghapus list sepenuhnya
 thislist4 = ["apple", "banana", "cherry"]
 thislist4.clear()
 print(thislist4)
+
+## Loop list
+# For Loop
+for x in thislist:
+  print(x) #value of list
+
+thislist5 = ["apple", "banana", "cherry"]
+for i in range(len(thislist5)):
+  print(thislist5[i]) #mengacu pada nomor indeksnya
+
+# While Loop => mengacu pada indeks
+i = 0
+while i < len(thislist):
+  print("ini while loop",thislist[i])
+  i = i + 1
+
+# menggunakan comprehensive list
+[print("ini comprehensive list",x) for x in thislist]
+
+## List Comprehensive
+# tanpa List Comprehensive
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print("Tanpa Comprehensive",newlist)
+
+# Dengan List Comprehensive
+newlist = [x for x in fruits if "a" in x] # dengan kondisi
+newlist1 = [x for x in range(10)] # dengan range
+newlist2 = [x for x in range(10) if x < 5] # dengan range dan kondisi
+print("Dengan Comprehensive",newlist)
+
+# Memanipulasi hasil dengan expresi
+newlist3 = [x.upper() for x in fruits] # menjadi huruf kapital
+newlist4 = ['hello' for x in fruits] # merubah semua nilai menjadi hello
+newlist5 = [x if x != "banana" else "orange" for x in fruits] # "Return the item if it is not banana, if it is banana return orange".
+
+## Sort List
+thislist.sort()
+print(thislist)
+
+thislist.sort(reverse = True)
+print(thislist)
+
+#dengan fungsi
+def myfunc(n):
+  return abs(n - 50)
+
+thisnumlist = [100, 50, 65, 82, 23]
+thisnumlist.sort(key = myfunc)
+print(thisnumlist)
+
+# agar tidak case sensitive
+thislist.sort(key = str.lower)
+print(thislist)
+
+# membalik urutan list
+thislist5.reverse()
+print(thislist5)
+
+## Copy List
+# Copy
+thiscopylist = thislist.copy()
+print(thiscopylist)
+
+#list
+thislistlist = list(thislist)
+print(thislistlist)
+
+## Join List
+# dengan operator +
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
+# dengan append
+for x in list2:
+  list1.append(x)
+
+print(list1)
+
+# dengan extend
+list1.extend(list2)
+print(list1)
